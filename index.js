352a655f5d85c9380857a12ae30d8d8d292c2404
Paul Hawxby <phawxby@gmail.com>
2017-09-26 13:38:20 +0100

    Better error handling

--- index.js
+++ index.js
@@ -24,62 +24,69 @@ module.exports = (url, opts, cb) => {
 
   puppeteer.launch().then(async (browser) => 
   {
-    const page = await browser.newPage();
-    
-    if (opts.viewport) {
-      await page.setViewport(opts.viewport);
-    }
-  
-    await page.goto(url);
-    await page.injectFile('node_modules/accessibility-developer-tools/dist/js/axs_testing.js');
-    
-    const {audit, report} = await page.evaluate(() => 
+    try 
     {
-      const auditConfig = new axs.AuditConfiguration();
-      auditConfig.scope = document.body
+      const page = await browser.newPage();
+      
+      if (opts.viewport) {
+        await page.setViewport(opts.viewport);
+      }
+    
+      await page.goto(url);
+      await page.injectFile('node_modules/accessibility-developer-tools/dist/js/axs_testing.js');
+      
+      const {audit, report} = await page.evaluate(() => 
+      {
+        const auditConfig = new axs.AuditConfiguration();
+        auditConfig.scope = document.body
 
-      const results = axs.Audit.run(auditConfig);      
+        const results = axs.Audit.run(auditConfig);      
 
-      const audit = results.map(function (result) {
-        const DOMElements = result.elements;
-        const message = '';
+        const audit = results.map(function (result) {
+          const DOMElements = result.elements;
+          const message = '';
 
-        if (DOMElements !== undefined) {
-          for (let i = 0; i < DOMElements.length; i++) {
-            const el = DOMElements[i];
-            message += '\n';
-            // Get query selector not browser independent. catch any errors and
-            // default to simple tagName.
-            try {
-              message += axs.utils.getQuerySelectorText(el);
-            } catch (err) {
-              message += ' tagName:' + el.tagName;
-              message += ' id:' + el.id;
+          if (DOMElements !== undefined) {
+            for (let i = 0; i < DOMElements.length; i++) {
+              const el = DOMElements[i];
+              message += '\n';
+              // Get query selector not browser independent. catch any errors and
+              // default to simple tagName.
+              try {
+                message += axs.utils.getQuerySelectorText(el);
+              } catch (err) {
+                message += ' tagName:' + el.tagName;
+                message += ' id:' + el.id;
+              }
             }
           }
-        }
 
-        return {
-          code: result.rule.code,
-          heading: result.rule.heading,
-          result: result.result,
-          severity: result.rule.severity,
-          url: result.rule.url,
-          elements: message
+          return {
+            code: result.rule.code,
+            heading: result.rule.heading,
+            result: result.result,
+            severity: result.rule.severity,
+            url: result.rule.url,
+            elements: message
+          };
+        });
+
+        const result = {
+          audit: audit,
+          report: axs.Audit.createReport(results)
         };
+
+        return JSON.parse(JSON.stringify(result));      
       });
 
-      const result = {
-        audit: audit,
-        report: axs.Audit.createReport(results)
-      };
+      cb(null, audit, report);
 
-      return JSON.parse(JSON.stringify(result));      
-    });
-
-    await browser.close();
-
-    cb(null, audit, report);
+    } catch (error) {
+      cb(error);
+    }
+    finally {
+      await browser.close();
+    }
   })
   .catch(async (error) => {
     cb(error);
